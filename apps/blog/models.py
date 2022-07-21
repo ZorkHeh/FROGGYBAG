23183a761a658cd526481b0b844df346fd214e4c
ZorkHeh <zork576@gmail.com>
2022-07-21 16:21:37 +0300

    blog-models-Article-update commit

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -30,7 +30,7 @@ class Article(models.Model):
         on_delete=models.SET_NULL,
         null=True
     )
-    tags = models.ManyToManyField(Tag, verbose_name='Тег')
+    tags = models.ManyToManyField(Tag, verbose_name='Тег', blank=True)
     title = models.CharField(verbose_name='Заголовок', max_length=255)
     text_preview = models.TextField(verbose_name='Текс-превью')
     text = models.TextField(verbose_name='Текст')
