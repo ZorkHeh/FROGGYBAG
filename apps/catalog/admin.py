7b719b58e0d2ce6c9f773c2465bea7575586da58
ZorkHeh <zork576@gmail.com>
2022-08-15 16:28:30 +0300

    Product-update

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from apps.catalog.models import Category, Product
 
 
-admin.site.register(Product)
-
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['name']}
+
+
+@admin.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
