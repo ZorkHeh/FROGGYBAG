b5cc7501e4f147bcaae5fa7d1d9cd10b68e6011e
ZorkHeh <zork576@gmail.com>
2022-09-20 17:27:45 +0300

    cart_view update

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from apps.order import views
 
 urlpatterns = [
-    path('', views.cart_view, name='cart_view'),
+    path('cart/', views.cart_view, name='cart_view'),
     path('add/', views.add_to_cart, name='add_to_cart')
 ]
