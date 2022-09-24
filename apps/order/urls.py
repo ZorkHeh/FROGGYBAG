0ce4226c22f9cd533f79ffc23a81d95e6abb2c8b
ZorkHeh <zork576@gmail.com>
2022-09-24 23:05:22 +0300

    Order product and product quantity

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -4,5 +4,6 @@ from apps.order import views
 urlpatterns = [
     path('cart/', views.cart_view, name='cart_view'),
     path('add/', views.add_to_cart, name='add_to_cart'),
-    path('create/', views.create_order, name='create_order')
+    path('create/', views.create_order, name='create_order'),
+    path('delete/<int:row_id>/', views.delete_from_cart, name='delete_from_cart')
 ]
